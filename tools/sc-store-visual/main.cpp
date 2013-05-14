978232127f7a1bd236a9c7ce8c3abbb992772112
Andrew Kondratovich <andrew.kondratovich@gmail.com>
2013-05-14 18:06:35 +0300

    reset main.cpp

--- tools/sc-store-visual/main.cpp
+++ tools/sc-store-visual/main.cpp
@@ -1,10 +1,9 @@
 #include <QtGui/QApplication>
-
 #include "mainwindow.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QApplication a(argc, argv);
-
     MainWindow w;
     w.show();
 
