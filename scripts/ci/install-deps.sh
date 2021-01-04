20b0f309d54c74012045c3a979a17fb2c59199cf
Denis Koronchik <denis.koronchik@gmail.com>
2021-01-04 22:38:27 +0300

    [ci] Migrate to github actions

---
+++ scripts/ci/install-deps.sh
@@ -0,0 +1,3 @@
+sudo apt install python3-pip g++ librocksdb-dev libglib2.0-dev qt5-qmake qtbase5-dev \ 
+                 libboost-system-dev libboost-filesystem-dev libboost-program-options-dev python3-setuptools python3 \
+                 make cmake antlr libcurl4-openssl-dev libclang-7-dev llvm-7
--- scripts/ci/install-tests.sh
+++ scripts/ci/install-tests.sh
@@ -7,7 +7,6 @@ pip3 install --user -r requirements.txt
 mkdir build
 pushd build
 
-export CC=${CCOMPILER} CXX=${CXXCOMPILER}
 cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DSC_AUTO_TEST=ON -DSC_BUILD_SCTP=ON -DBUILD_TESTING=ON -DSC_KPM_SCP=OFF
 echo "travis_fold:start:MAKE"
 make -j${JOBS}
