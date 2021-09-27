9e9cebf8dd38a7102137e77a2f1a8a9bb2451f28
fintarin <valukevichv@gmail.com>
2021-09-28 00:11:13 +0300

    [ci][cmake] Setup coverage and sanitizers builds #377

--- scripts/ci/install-tests.sh
+++ scripts/ci/install-tests.sh
@@ -7,7 +7,7 @@ pip3 install --user -r requirements.txt
 mkdir build
 pushd build
 
-cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DSC_USE_SANITIZER=${SANITIZER_TYPE} -DSC_AUTO_TEST=ON -DSC_BUILD_SCTP=OFF -DBUILD_TESTING=ON -DSC_KPM_SCP=OFF
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DSC_USE_SANITIZER=${SANITIZER_TYPE} -DSC_COVERAGE=${COVERAGE} -DSC_AUTO_TEST=ON -DSC_BUILD_SCTP=OFF -DBUILD_TESTING=ON -DSC_KPM_SCP=OFF
 make -j4
 
 ../bin/sc-builder -i ../tools/builder/tests/kb -o ../bin/sc-builder-test-repo -c -f
