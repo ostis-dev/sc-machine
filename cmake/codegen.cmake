f7b0bef7a2c009e3ab94b664b363c1fb4979907c
fintarin <valukevichv@gmail.com>
2021-10-19 09:48:04 +0300

    [cmake] Remove old code for Windows support

--- cmake/codegen.cmake
+++ cmake/codegen.cmake
@@ -12,55 +12,31 @@ macro(sc_codegen_ex Target SrcPath OutputPath)
           set(META_FLAGS ${META_FLAGS} "-I${dir}")
     endforeach()
 
-    # include the system directories
-    if (MSVC)
-        # assume ${VS_VERSION} is the version of Visual Studio being used to compile
-        set(META_FLAGS ${META_FLAGS} 
-            "-IC:/Program Files (x86)/Microsoft Visual Studio ${VS_VERSION}/VC/include"
-        )
-    elseif (UNIX)
-        set(META_FLAGS ${META_FLAGS}
-            "-I${LIBCLANG_LIBDIR}/clang/${LIBCLANG_VERSION_STRING}/include/"
-        )
-    else ()
-        # you can figure it out for other compilers :)
-        message(FATAL_ERROR "System include directories not implemented for this compiler.")
-    endif ()
 
-    if (MSVC)
-        add_custom_command(
-            PRE_BUILD
-            TARGET ${Target}
-            COMMAND call "${SC_CODEGEN_TOOL}"
-            --target     "${Target}"
-            --source     "${SrcPath}"
-            --output     "${OutputPath}"
-            --flags      "${META_FLAGS}"
-            --build_dir  "${CMAKE_CURRENT_BINARY_DIR}"
-            --cache
-        )
-    else()
-        file(GLOB_RECURSE HEADER_FILES CONFIGURE_DEPENDS "${SrcPath}/*.hpp")
+    set(META_FLAGS ${META_FLAGS}
+        "-I${LIBCLANG_LIBDIR}/clang/${LIBCLANG_VERSION_STRING}/include/"
+    )
 
-        set (CACHE_FILE "${CMAKE_CURRENT_BINARY_DIR}/${Target}.gen_cache")
-        add_custom_command(
-            OUTPUT ${CACHE_FILE}
-            COMMAND "${SC_CODEGEN_TOOL}"
-            --target     "${Target}"
-            --source     "${SrcPath}"
-            --output     "${OutputPath}"
-            --build_dir  "${CMAKE_CURRENT_BINARY_DIR}"
-            --flags      "'${META_FLAGS}'"
-            --cache
-            DEPENDS ${HEADER_FILES}
-        )
+    file(GLOB_RECURSE HEADER_FILES CONFIGURE_DEPENDS "${SrcPath}/*.hpp")
 
-        set (SUB_TARGET "${Target}-GenerateCode")
-        add_custom_target(${SUB_TARGET} DEPENDS ${CACHE_FILE} SOURCES ${HEADER_FILES})
+    set (CACHE_FILE "${CMAKE_CURRENT_BINARY_DIR}/${Target}.gen_cache")
+    add_custom_command(
+        OUTPUT ${CACHE_FILE}
+        COMMAND "${SC_CODEGEN_TOOL}"
+        --target     "${Target}"
+        --source     "${SrcPath}"
+        --output     "${OutputPath}"
+        --build_dir  "${CMAKE_CURRENT_BINARY_DIR}"
+        --flags      "'${META_FLAGS}'"
+        --cache
+        DEPENDS ${HEADER_FILES}
+    )
 
-        add_dependencies(${SUB_TARGET} sc-code-generator)
-        add_dependencies(${Target} ${SUB_TARGET})
-    endif()
+    set (SUB_TARGET "${Target}-GenerateCode")
+    add_custom_target(${SUB_TARGET} DEPENDS ${CACHE_FILE} SOURCES ${HEADER_FILES})
+
+    add_dependencies(${SUB_TARGET} sc-code-generator)
+    add_dependencies(${Target} ${SUB_TARGET})
 
     target_include_directories(${Target} PUBLIC ${OutputPath})
 
